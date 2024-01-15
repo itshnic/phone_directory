080375cabf326c4a45ced0513a0143c245ac7fba
Itshnic72 <rv920490@gmail.com>
2024-01-15 17:43:28 +0500

    update

--- phone_dir_module/model.py
+++ phone_dir_module/model.py
@@ -3,33 +3,18 @@ file='phone_dir.txt'
 cache_list = []
 SEPARATOR=';'
 
-def add_user(list_request: list, text_request: str) ->list:
-  new_contact=[]
-  for i in list_request:
-    new_contact.append(checkNull(input(f'Введите {i}: ')))
-  return new_contact
-  
-
-def to_list(file, flag):
-  base_data = []
+def to_list(file:str, flag:str,hat:list):
   if os.path.isfile(file):
     with open(file, flag,encoding='utf-8') as data:
-      for line in data:
-        base_data.append(line.split(SEPARATOR))
+      base_data={i:item for i, item in enumerate(list(map(lambda x: x.strip().split(SEPARATOR), data)),1)}
       print(base_data)
-  return base_data
+      return base_data
 
 def to_file(file, cache_list, flag):
   with open(file, flag, encoding='utf-8') as data:
     for i in cache_list:
-      data.write(f'{" ".join(i)}\n')
+      data.write(f'{SEPARATOR.join(i)}\n')
   data.close()
-  
-def checkNull(item):
-  if item:
-    return item
-  else:
-    return 'Null'
 
 def search(list):
   index_true = []
--- phone_dir_module/view.py
+++ phone_dir_module/view.py
@@ -5,12 +5,13 @@ def print_message(message:str):
   print(f' {message}')
   print('-'*(len(message)+2))
 
-def max_len_item_dict(dict_txt)->list:
-  return [len(max(dict_txt.keys(),key=len)),len(max(dict_txt.values(),key=len))]
+def max_len_item_dict(list_txt)->list:
+  if type(list_txt.values())==list:
+    return [map(lambda x:len(max(x, key=len)),list(zip(*list_txt.values())))]
+  else:
+    return [len(max(list_txt.keys(),key=len)),len(max(list_txt.values(),key=len))]
   
-def max_len_item_list(dict_txt)->list:
-  return [len(max(dict_txt.keys(),key=len)),len(max(dict_txt.values(),key=len))]
-
+  
 def panel(text_1,text_2,dict_txt):
   print_message(text_1)
   max_size=max_len_item_dict(dict_txt)
@@ -18,4 +19,17 @@ def panel(text_1,text_2,dict_txt):
     print(f'  {i:<{max_size[0]}} -> {f}')
   return input(f'\n{text_2} ')
 
-  
+def checkNull(item):
+  if item:
+    return item
+  else:
+    return 'Null'
+
+def add_user(list_request: list, text_request: str) ->list:
+  new_contact=[]
+  for user_info in list_request:
+    new_contact.append(checkNull(input(f'{text_request} {user_info}: ')))
+  return new_contact
+
+def input_request(user_info, text_request):
+  input(f'{text_request} {user_info}: ')
